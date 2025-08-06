451fdc0c46d72080dc00ebeaa794bca62a0e2dc9
vikrant saini <vikrantsaini094@gmail.com>
2025-08-06 11:19:34 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import sys
+import logging
+
+def error_meassgae_details(error, error_detail:sys):
+
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "error occured in python script name [{0}] line number [{}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+
+class customException(Exception):
+
+    def __init__(self, error_message, error_detail : sys):
+
+        super.__init__(error_message)
+        self.error_message = error_meassgae_details(error_message, error_detail = error_detail)
+
+    
+    def __str__(self):
+        return self.error_message
+
+if __name__=="__main__":
+
+    try :
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise customException(e, sys)
+        
